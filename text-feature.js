259ae6312c4c949611e51d4bb9ec753661024595
John Mark Acuesta <acuestajohnmark@gmail.com>
2019-04-29 18:16:30 +0800

    Edited the text feature

--- text-feature.js
+++ text-feature.js
@@ -25,40 +25,12 @@ define([], function() {
         
     }
 
-    function postSourcing(context) {
-        
-    }
-
-    function lineInit(context) {
-        
-    }
-
-    function validateDelete(context) {
-        
-    }
-
-    function validateInsert(context) {
-        
-    }
-
-    function validateLine(context) {
-        
-    }
-
-    function sublistChanged(context) {
-        
-    }
+   
 
     return {
         pageInit: pageInit,
         saveRecord: saveRecord,
         validateField: validateField,
-        fieldChanged: fieldChanged,
-        postSourcing: postSourcing,
-        lineInit: lineInit,
-        validateDelete: validateDelete,
-        validateInsert: validateInsert,
-        validateLine: validateLine,
-        sublistChanged: sublistChanged
+        fieldChanged: fieldChanged
     };
 });
