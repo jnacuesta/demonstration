11a19c4b1bcd24157ae9331d486ab7505fead90f
John Mark Acuesta <acuestajohnmark@gmail.com>
2019-04-29 17:54:37 +0800

    Added three new modules for our existing project

---
+++ module2.js
@@ -0,0 +1,19 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType Suitelet
+ * @NModuleScope Public
+ * @Author JM
+ * @Description <Insert Description Here>
+ * Version	Date		Author	Remarks
+ * 1.00		29 Apr 2019	TC-017	Initial Version
+ */
+define([], function() {
+
+    function onRequest(context) {
+        
+    }
+
+    return {
+        onRequest: onRequest
+    };
+});
---
+++ module3.js
@@ -0,0 +1,29 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType UserEventScript
+ * @NModuleScope Public
+ * @Author JM
+ * @Description <Insert Description Here>
+ * Version	Date		Author	Remarks
+ * 1.00		29 Apr 2019	TC-017	Initial Version
+ */
+define([], function() {
+
+    function beforeLoad(context) {
+        
+    }
+
+    function beforeSubmit(context) {
+        
+    }
+
+    function afterSubmit(context) {
+        
+    }
+
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    };
+});
