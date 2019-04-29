4c3c8a384d027c8a2a6279ba9e476fc749ff2289
John Mark Acuesta <acuestajohnmark@gmail.com>
2019-04-29 17:58:41 +0800

    Added module 4 to our existing project

---
+++ module4.js
@@ -0,0 +1,19 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType ScheduledScript
+ * @NModuleScope Public
+ * @Author JM
+ * @Description <Insert Description Here>
+ * Version	Date		Author	Remarks
+ * 1.00		29 Apr 2019	TC-017	Initial Version
+ */
+define([], function() {
+
+    function execute(context) {
+        
+    }
+
+    return {
+        execute: execute
+    };
+});
